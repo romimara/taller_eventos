02278f0ee6a462bf202a675b3993aa43ae378e25
romimara <r.martinezravelli@gmail.com>
2023-11-29 01:53:10 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -3,3 +3,7 @@ let div = document.getElementById('div');
 div.addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+boton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    lanzarAlerta();
+});
